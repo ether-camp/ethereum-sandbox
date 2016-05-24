ded4eb97aa9bc4e264cbb16cd10f51fc8b635473
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2016-05-24 19:09:53 +0000

    Add scripts to package sandbox; Allow using of the sandbox as a lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,11 +13,29 @@ module.exports = function(grunt) {
     simplemocha: {
       control: { src: ['specs/sandbox-control.spec.js'] },
       jsonRpc: { src: ['specs/sandbox-json-rpc.spec.js'] }
+    },
+    obfuscator: {
+      files: [
+        'app.js',
+        'compiler.js',
+        'plugins.js',
+        'sandbox_control.js',
+        'util.js',
+        'api/*.js',
+        'ethereum/*.js',
+        'sandbox/*.js',
+        'types/*.js'
+      ],
+      entry: 'app.js',
+      out: 'ethereum-sandbox.js',
+      strings: true,
+      root: __dirname
     }
   });
 
   grunt.loadNpmTasks('grunt-run');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-obfuscator');
 
   grunt.registerTask('simplemochaGrep', function() {
     grunt.config('simplemocha.options.grep', grunt.option('grep'));
--- app.js
+++ app.js
@@ -11,39 +11,45 @@ var plugins = require('./plugins');
 
 plugins.load(events);
 
-app.use(cors());
-app.use(bodyParser.json());
-app.post('/sandbox', function(req, res) {
-  control.create(req.query.id, req.body, function(err, instance) {
-    if (err) res.status(500).send(err);
-    else res.json({ id: instance.id });
+function start(cb) {
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.post('/sandbox', function(req, res) {
+    control.create(req.query.id, req.body, function(err, instance) {
+      if (err) res.status(500).send(err);
+      else res.json({ id: instance.id });
+    });
   });
-});
-app.post('/sandbox/:id', function(req, res, next) {
-  if (!control.contains(req.params.id)) res.sendStatus(404);
-  else control.instance(req.params.id).middleware(req, res, next);
-});
-app.delete('/sandbox/:id', function(req, res, next) {
-  if (!control.contains(req.params.id)) res.sendStatus(404);
-  else {
-    control.stop(req.params.id, function(err) {
+  app.post('/sandbox/:id', function(req, res, next) {
+    if (!control.contains(req.params.id)) res.sendStatus(404);
+    else control.instance(req.params.id).middleware(req, res, next);
+  });
+  app.delete('/sandbox/:id', function(req, res, next) {
+    if (!control.contains(req.params.id)) res.sendStatus(404);
+    else {
+      control.stop(req.params.id, function(err) {
+        if (err) res.status(500).send(err);
+        else res.sendStatus(200);
+      });
+    }
+  });
+  app.get('/sandbox', function(req, res) {
+    res.json(_.keys(control.instances));
+  });
+  app.post('/reset', function(req, res) {
+    control.reset(function(err) {
       if (err) res.status(500).send(err);
       else res.sendStatus(200);
     });
-  }
-});
-app.get('/sandbox', function(req, res) {
-  res.json(_.keys(control.instances));
-});
-app.post('/reset', function(req, res) {
-  control.reset(function(err) {
-    if (err) res.status(500).send(err);
-    else res.sendStatus(200);
   });
-});
+  
+  var server = app.listen(8555, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Sandbox is listening at http://%s:%s', host, port);
+    if (cb) cb();
+  });
+}
 
-var server = app.listen(8555, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Sandbox is listening at http://%s:%s', host, port);
-});
+if (require.main === module) start();
+else module.exports = { start: start };
